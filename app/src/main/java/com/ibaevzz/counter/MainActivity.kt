495886f35fb0577249c91d463f80b4db92530439
Ibaev Zubai <ibaevzubic2004@gmail.com>
2023-12-13 23:53:19 +0300

    Завершена работа над UI главного Activity

--- app/src/main/java/com/ibaevzz/counter/MainActivity.kt
+++ app/src/main/java/com/ibaevzz/counter/MainActivity.kt
@@ -2,10 +2,15 @@ package com.ibaevzz.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ibaevzz.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
